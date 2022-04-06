258c3efd145c7ab69bb54ea128cb3510e2f558eb
abuabdulrahaman <abuabdulrahaman21@gmail.com>
2022-04-06 15:49:47 +0000

    firstcommit

---
+++ results.js
@@ -0,0 +1,9 @@
+const results = [
+  { name: "Windy Dari", class: "Form One", marks: 8 },
+  { name: "Windy Dari", class: "Form One", marks: 8 },
+  { name: "Abu Dari", class: "Form One", marks: 8 },
+  { name: "Ama Dari", class: "Form One", marks: 9 },
+  { name: "Kofi Dari", class: "Form One", marks: 7 }
+]
+
+module.exports=results
